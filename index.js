f182f8102b7ffd67632dc252ac105fe688f399fa
Aloklaha <alok.laha@tier5.in>
2024-01-18 11:36:17 +0530

    Rearrangement of payload data and adding payload validation

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const {v4} = require("uuid")
 
 const PORT = 4050;
 app.use(express.json())
 
 app.post("/create-logs", async(req, res) => {
-    // This API will accept the required parameter to create log files within a specific directory.
-    // Directory structure will be date-> org_name -> module.log
-    // file names will be date|org_name|module.log
+    // This API will payload and log in the file
 
-    if(reqValidator(req.body)){
-        let data = {
-            "id": v4(),
-            "type": req.body.type,
-            "desc": "Collected $10 from Brian Debes for Kick Boxing from Card",
-            "service": "payment_initiate",
-            "module": req.body.module,
-            "orgCode": req.body.orgCode,
-            "createdBy": "brian",
-            "data": {
-              "request": {}, // Request body of the service
-              "response": {} // Response from the service
-            }
-          }
-
-        // Now we need to check wheather this file exists or not. If not then create or update the content
+    if(reqValidator(req.body.data)){
         let date = new Date();
-        // let today = date.getFullYear()+"-"+date.getMonth()+1+"-"+date.getDate();
+        let today = date.getFullYear()+"-"+date.getMonth()+1+"-"+date.getDate();
         let time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-        let today = req.body.today;
-        const filePath = `/log/${today}|${req.body.orgCode}|${req.body.module}.log`;
+        const filePath = `/log/${today}|${req.body.data.orgCode}|${req.body.data.module}.log`;
 
         // Append new content to the file
-        fs.appendFileSync(__dirname+filePath, `[${time}]: `+"'"+JSON.stringify(data)+"'"+'\n\n', 'utf8');
+        fs.appendFileSync(__dirname+filePath, `[${time}]: `+"'"+JSON.stringify(req.body.data)+"'"+'\n\n', 'utf8');
 
-        return res.status(200).json({message: "Success", body: req.body})   
+        return res.status(200).json({message: "Success", body: req.body.data})   
     }
     else
-    return res.status(400).json({message: "Failed", body: req.body})   
+    return res.status(400).json({message: "Failed", body: req.body.data})   
 
 });
 
@@ -49,7 +30,7 @@ app.get("/", (req, res)=>{
 
 // Validate request parameters
 const reqValidator = (body) => {
-    return body !== null ? true : false;
+    return ((body.orgCode !== null && body.orgCode !== undefined && body.orgCode !== "") && (body.module !== null && body.module !== undefined && body.module !== "")) ? true : false;
 }
 
 
